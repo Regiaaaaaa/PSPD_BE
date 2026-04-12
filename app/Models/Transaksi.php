6dee06814e4670ca1ba1999e9b6ea9394e7efe15
Regiaaaaaa <gheizarafashaquille@gmail.com>
2026-04-12 22:26:00 +0700

    feat: feat realtime denda with appends

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,67 +4,74 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Transaksi extends Model
 {
     use HasFactory;
 
     protected $table = 'transaksi';
+
     protected $fillable = [
         'user_id',
         'buku_id',
-
-        'jumlah',
         'kepentingan',
         'pesan_ditolak',
-
-
         'tgl_pinjam',
         'tgl_deadline',
         'tgl_kembali',
-
-
         'status',
-
-
         'disetujui_oleh',
         'diterima_oleh',
         'ditolak_oleh',
     ];
 
-        protected $casts = [
-        'tgl_pinjam' => 'date',
+    protected $casts = [
+        'tgl_pinjam'   => 'date',
         'tgl_deadline' => 'date',
-        'tgl_kembali' => 'date',
+        'tgl_kembali'  => 'date',
     ];
+
+    protected $appends = ['denda_berjalan'];
+
+    public function getDendaBerjalanAttribute() 
+    {
+        if ($this->status === 'dipinjam' && !$this->tgl_kembali) {
+            
+            $deadline = Carbon::parse($this->tgl_deadline)->startOfDay();
+            $hariIni = now()->startOfDay();
+            if ($hariIni->gt($deadline)) {
+                $selisihHari = $deadline->diffInDays($hariIni);
+                return $selisihHari * 1000; 
+            }
+        }
+        
+        return 0;
+    }
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function buku()
     {
-        return $this->belongsTo(Buku::class);
+        return $this->belongsTo(Buku::class, 'buku_id');
     }
 
-    // Operator yang menyetujui
     public function disetujuiOleh()
     {
-    return $this->belongsTo(User::class, 'disetujui_oleh');
+        return $this->belongsTo(User::class, 'disetujui_oleh');
     }
 
-
-    // Operator yang menerima pengembalian
     public function diterimaOleh()
     {
-    return $this->belongsTo(User::class, 'diterima_oleh');
+        return $this->belongsTo(User::class, 'diterima_oleh');
     }
-
-    // Operator yang menolak
+    
     public function ditolakOleh()
     {
-    return $this->belongsTo(User::class, 'ditolak_oleh');
+        return $this->belongsTo(User::class, 'ditolak_oleh');
     }
 
     public function denda()
@@ -72,4 +79,3 @@ class Transaksi extends Model
         return $this->hasOne(Denda::class);
     }
 }
-
